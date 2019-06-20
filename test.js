adb55e3c33ab1a1623b9db7be265a0848efce848
Abhishek Pandya <40919306+abhishekp106@users.noreply.github.com>
2019-06-20 12:42:23 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+import uvicorn
+
+app = Starlette(debug=True)
+
+@app.route("/classify-url", methods=["GET"])
+async def classify_url(request):
+    bytes = await get_bytes(request.query_params["url"])
+    img = open_image(BytesIO(bytes))
+    _,_,losses = learner.predict(img)
+    return JSONResponse({
+        "predictions": sorted(
+            zip(cat_learner.data.classes, map(float, losses)),
+            key=lambda p: p[1],
+            reverse=True
+        )
+    })
